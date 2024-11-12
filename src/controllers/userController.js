e1ba22f06c574fe77d4949106d31fa1312908688
Peter <59800244+PetersGlory@users.noreply.github.com>
2024-11-12 23:20:29 +0100

    correction

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@
 const jwt = require('jsonwebtoken');
 const logger = require('../utils/logger');
 const User = require('../models/User');
+const Wallet = require('../models/Wallet');
 
 const userController = {
   async register(req, res) {
